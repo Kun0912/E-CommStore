a4abdf81f1e367303e2247678db405783a543285
Chinu Gunjal <79391691+Kun0912@users.noreply.github.com>
2022-05-15 18:08:18 +0530

    Add files via upload

---
+++ sellerportal/admin_page.php
@@ -0,0 +1,120 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['add_product'])){
+
+   $seller_name = $_POST['seller_name'];
+   $username = $_POST['username'];
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_FILES['product_image']['name'];
+   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
+   $product_image_folder = 'uploaded_img/'.$product_image;
+
+   if(empty($seller_name) || empty($username) ||empty($product_name) || empty($product_price) || empty($product_image)){
+      $message[] = 'please fill out all';
+   }else{
+      $insert = "INSERT INTO products(seller_name,username,name, price, image) VALUES('$seller_name','$username','$product_name', '$product_price', '$product_image')";
+      $upload = mysqli_query($conn,$insert);
+      if($upload){
+         move_uploaded_file($product_image_tmp_name, $product_image_folder);
+         $message[] = 'new product added successfully';
+      }else{
+         $message[] = 'could not add the product';
+      }
+   }
+
+};
+
+if(isset($_GET['delete'])){
+   $id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM products WHERE id = $id");
+   header('location:admin_page.php');
+};
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<span class="message">'.$message.'</span>';
+   }
+}
+
+?>
+   
+<div class="container">
+
+   <div class="admin-product-form-container">
+
+      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+         <h3>add a new product</h3>
+         <input type="text" placeholder="Seller Name" name="seller_name" class="box">
+         <input type="text" placeholder="Enter Your Username" name="username" class="box">
+         <input type="text" placeholder="Enter product name" name="product_name" class="box">
+         <input type="number" placeholder="Enter product price" name="product_price" class="box">
+         <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
+         <input type="submit" class="btn" name="add_product" value="add product">
+      </form>
+
+   </div>
+
+   <?php
+
+   $select = mysqli_query($conn, "SELECT * FROM products");
+   
+   ?>
+   <div class="product-display">
+      <table class="product-display-table">
+         <thead>
+         <tr>
+            <th>Seller Name</th>
+            <th>Username</th>
+            <th>product image</th>
+            <th>product name</th>
+            <th>product price</th>
+            <th>action</th>
+         </tr>
+         </thead>
+         <?php while($row = mysqli_fetch_assoc($select)){ ?>
+         <tr>
+            <td><?php echo $row['seller_name']; ?></td>
+            <td><?php echo $row['username']; ?></td>
+            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="verify.php?edit=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-edit"></i>Edit</a>
+               <!-- <a href="admin_page.php?delete=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-trash"></i> delete </a> -->
+             
+            </td>
+         </tr>
+      <?php } ?>
+      </table>
+   </div>
+
+</div>
+
+
+</body>
+</html>
